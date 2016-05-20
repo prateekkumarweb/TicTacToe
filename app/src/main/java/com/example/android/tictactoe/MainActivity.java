dbf3519a7f57315c151247d81811e2e7e146ee04
Prateek Kumar <pksingh1023@gmail.com>
2016-05-20 22:04:52 +0530

    updated project

--- app/src/main/java/com/example/android/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/android/tictactoe/MainActivity.java
@@ -14,6 +14,7 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 
+// Updated to compile this activity
 public class MainActivity extends AppCompatActivity {
 
     TicTacToeGame ticTacToeGame;
